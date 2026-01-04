286b810dbeef03342f6ad1d77076f2feecbff1a5
Sania Singh <saniasingh2003@icloud.com>
2026-01-04 14:25:00 +0530

    Initial Commit

---
+++ journal/journalApp.swift
@@ -0,0 +1,17 @@
+//
+//  journalApp.swift
+//  journal
+//
+//  Created by Sania Singh on 04/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct journalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
